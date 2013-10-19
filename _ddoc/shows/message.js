3f486ea0c0d2ee778fb629ed66cce09f1cb72094
Elizabeth Marston <beth@undoware.ca>
2013-10-18 18:55:56 -0700

    autocommit

--- _ddoc/shows/message.js
+++ _ddoc/shows/message.js
@@ -1,14 +1,14 @@
 function(doc,req) {
-  var attachments = false;
+  var attachment = false;
   var html = '<html><body><ul>';
-  if ('attachments' in req) { attachments = true; }
+  if ('attachment' in req) { attachment = true; }
 
 
   html += '<li><strong>id</strong>: ' + doc._id + '</li>';
   html += '<li><strong>sender</strong>: ' + doc.sender + '</li>';
   html += '<li><strong>text</strong>: ' + doc.text + '</li>';
 
-  if (attachments) {
+  if (attachment) {
     html += '<li><strong>attachment</strong>: <a href=#notyetimpl>download</a></li>';
   }
 
