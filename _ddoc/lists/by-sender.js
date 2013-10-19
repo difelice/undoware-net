72cca3547189fc0146f89c0208e1e0e86d4df0ab
Elizabeth Marston <beth@undoware.ca>
2013-10-19 14:26:11 -0700

    autocommit

--- _ddoc/lists/by-sender.js
+++ _ddoc/lists/by-sender.js
@@ -2,13 +2,13 @@ function(doc,req) {
   provides("html", function() {
     var html = "<html><body><table class='ink-table alternating'>";
         html+= "<thead><tr><th data-sortable='true'>Date</th><th>Message</th><th data-sortable='true'>Upload</th><th data-sortable='true'>Sender</th></tr></thead>";
+
     var mk_a = function(v) {
       var icon = '<i class="icon-file icon-2x"></i>';
-      if ('attachment' in v._attachments) {
-        var tip = '<a href="';
-        var out = '/_db/' + v._id;
-        var tail = '/attachment">' +  icon + '</a>';
-        return tip + out + tail;
+      if (true) {
+        var tip =  '<a href="' +  '/_db/' + v._id + '/attachment">';
+        var tail = '</a>';
+        return tip + icon + tail;
       } else {
         return icon;
       }
@@ -28,7 +28,7 @@ function(doc,req) {
          ) {
         html += '<tr><td>' + dater(row.value)    + '</td>';     /* first column: date */
         html +=     '<td>' + row.value.text      + '</td>';     /* 2nd: message */
-        html +=     '<td>' + mk_a(row.value) + '</td>';     /* 3rd; attachment */
+        html +=     '<td>' + mk_a(row.value)     + '</td>';     /* 3rd; attachment */
         html +=     '<td>' + row.value.sender    + '</td>';     /* Fourth: sender */
         html += '</tr>'; /* done row */
       }
