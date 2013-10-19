79f58aaf9ad1917eba4192346fc96be704ff664a
Elizabeth Marston <beth@undoware.ca>
2013-10-18 18:17:49 -0700

    autocommit

--- _ddoc/lists/by-sender.js
+++ _ddoc/lists/by-sender.js
@@ -2,7 +2,9 @@ function(doc,req) {
   provides("html", function() {
     var html = "<html><body><table class='ink-table alternating'>";
     while (row = getRow()) {
-      html += '<tr><td>' + row.key + '</td><td>' + row.value.text + '</td></tr>';
+      if (row.query.sender === row.value.sender) {
+        html += '<tr><td>' + row.key + '</td><td>' + row.value.text + '</td></tr>';
+      }
     }
     html += "</table></body></html>";
     return html;
