0533aaf3898d028d793d117ffd6fbc4b9c98e876
Elizabeth Marston <beth@undoware.ca>
2013-10-19 13:36:24 -0700

    autocommit

--- _ddoc/lists/by-sender.js
+++ _ddoc/lists/by-sender.js
@@ -10,9 +10,7 @@ function(doc,req) {
 
     var dater = function(v) {
       var out = '';
-      try {
-        out = v.when[0] + '/' + v.when[1] + '/' + v.when[2]
-      } catch {}
+
       return out;
     };
 
