f9f8928299539e9dcc8662243ecd0f7b84c141ff
Elizabeth Marston <ms.liz.mars@gmail.com>
2016-10-02 00:21:57 -0700

    got gh pages working

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 //TODO gulp tasks for e.g. updating the cdn etc. 
  
 
-
+var build_location='dist'; 
+var gulp = require('gulp');
 var ethdeploy = require('ethelo-deploy');
+var del = require('del');
 gulp.task('deploy', function () {
-    return gulp.src('public')
-        .pipe(ethdeploy({ message: 'Deployed to liz-mars.github.io/undoware.net/' }));
+    del([`#{build_location}/**/.git/`]);
+    return gulp.src(build_location)
+        .pipe(ethdeploy({ message: 'Deployed to liz-mars.github.io/undoware-net/' }));
 });
